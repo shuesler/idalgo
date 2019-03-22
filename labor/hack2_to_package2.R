8401362c099690c8ae62195406bdd8a19e1879f9
shuesler <silvan.husler@unine.ch>
2019-03-22 16:08:10 +0100

    ff

--- labor/hack2_to_package2.R
+++ labor/hack2_to_package2.R
@@ -12,36 +12,69 @@
 #doll  <- magrittr::use_series
 #options(tibble.print_min = 100)
 ##########################################################################################################
-library(idalgo)
-
-# Vignetten - Beispiel
-
-fig1  <- graph.formula(W - +X, W - +Z, X - +Z, Z - +Y, X - +Y, Y - +X, simplify = FALSE)
-fig1  <- set.edge.attribute(graph = fig1, name = "description", index = c(5, 6), value = "U")
-
-#mat   <- matrix(nrow = 4,ncol = 2,byrow = T,
-#                data = c(
-#                  1, 1, # w
-#                  0, 0, # x
-#                  2, 0, # z
-#                  2, 3  # y
-#                ))
+#library(idalgo)
 #
-#row.names(mat) <- c("W", "X", "Z", "Y")
-
-NODES <- names(V(fig1))
-
-ce1   <- causal.effect(y = "Y", x = "X", z = NULL, G = fig1, expr = TRUE, steps = TRUE)
-
-alles <- ce1$steps
+## Vignetten - Beispiel
+#
+#fig1  <- graph.formula(W - +X, W - +Z, X - +Z, Z - +Y, X - +Y, Y - +X, simplify = FALSE)
+#fig1  <- set.edge.attribute(graph = fig1, name = "description", index = c(5, 6), value = "U")
+#
+##mat   <- matrix(nrow = 4,ncol = 2,byrow = T,
+##                data = c(
+##                  1, 1, # w
+##                  0, 0, # x
+##                  2, 0, # z
+##                  2, 3  # y
+##                ))
+##
+##row.names(mat) <- c("W", "X", "Z", "Y")
+#
+#NODES <- names(V(fig1))
+#
+#ce1   <- causal.effect(y = "Y", x = "X", z = NULL, G = fig1, expr = TRUE, steps = TRUE)
+#
+#alles <- ce1$steps
 ##########################################################################################################
 
 
+#Smoking - Beispiel
+#library(idalgo)
+#fig1    <- igraph::graph.formula(X -+ Z, Z -+ Y, X -+ Y, Y -+ X,simplify = FALSE)
+#fig1    <- igraph::set.edge.attribute(graph = fig1, name  = "description", index = c(3,4), value = "U")
+#
+#NODES   <- names(V(fig1))
+#
+#ce1     <- causal.effect(y = "Y", x = "X", z = NULL, G = fig1, expr = TRUE, steps = TRUE, primes = T)
+#
+#ce1$P %>% katexR::katex()
+#
+#alles   <- ce1$steps
 
+##########################################################################################################
+# Hernan 20.3
 
+# Auf Seite 19 (Teil III) steht:
+# Table 20.1 shows data from a sequentially randomized trial with treatmentconfounder
+# feedback, as represented by the causal diagram in Figure 20.3.
+library(idalgo)
 
+fig1  <- graph.formula(
+  L0 -+ A0,
+  A0 - +L1,
+  L1 - +A1,
+  L0 ++ Y,
+  L1 ++ Y,
+  simplify = FALSE)
 
+fig1  <- igraph::set.edge.attribute(graph = fig1, name  = "description",index = c(4,7), value = "U")
 
+NODES <- names(V(fig1))
+
+ce1   <- causal.effect(y="Y", x=c("A0", "A1"), G = fig1, primes = T, steps = T)
+
+ce1$P %>% katexR::katex()
+
+alles <- ce1$steps
 
 ##########################################################################################################
 
