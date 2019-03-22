65a548a9475063d0f5f4f08e2f87732f0c3d8a01
shuesler <silvan.husler@unine.ch>
2019-03-22 15:56:50 +0100

    ff

--- labor/beispiele.R
+++ labor/beispiele.R
@@ -1,3 +1,97 @@
+
+liste %>% names
+
+liii <- list()
+
+ppp <- function(l) {
+
+  liii <<- append(liii, names(l))
+
+  pruef <- map(l, is.list) %>% purrr::as_vector() %>% unname()
+
+
+  if(any(pruef) == FALSE) {
+    return(print("FERTIG"))
+  }
+
+
+  tmp <- list()
+  neu <- l[pruef]
+  for(i in seq_along(neu)) {
+
+    tmp <- append(tmp, neu[[i]])
+
+
+  }
+
+
+  ppp(tmp)
+
+
+}
+
+
+ppp(liste)
+
+
+library(tidyverse)
+library(rlang)
+
+
+liste <- list(
+  a = c(a1 = list("a1"), a2 = list("a2")),
+  nichts1 = 1,
+  b = c(b1 = list("b1"), b2 = list("b2")),
+  nichts2 = 2,
+  c = c(c1 = list("c1"), c2 = list("c2")),
+  nichts3 = 3,
+  d = c(d1 = list("d1"), d2 = list("d2")),
+  nichts4 = NULL,
+  e = c(e1 = list("e1"), e2 = list("e2")),
+  list(9, list(10, list(99), list(88)))
+)
+
+
+
+
+map(aaa, is.list) %>% purrr::as_vector() %>% unname()
+
+
+foo <- function(l){
+
+  listviewer::number_unnamed(l)
+
+  lapply(l,
+         function(x)
+           if(is.list(x) && length(x)==0) "nichts"
+         else
+           if(is.list(x)) foo(x)
+         else x)
+}
+
+
+
+foo(liste)
+listviewer::number_unnamed(liste)
+
+
+where <- function(name, env = caller_env()) {
+  if (identical(env, empty_env())) {
+    # Base case
+    stop("Can’t find", name, call. = FALSE)
+  } else if (env_has(env, name)) {
+    # Success case
+    env
+  } else {
+    # Recursive case
+    where(name, env_parent(env))
+  }
+}
+
+
+
+
+
 # ref(alles)
 
 ##########################################################################################################
--- labor/hack2_to_package2.R
+++ labor/hack2_to_package2.R
@@ -19,15 +19,15 @@ library(idalgo)
 fig1  <- graph.formula(W - +X, W - +Z, X - +Z, Z - +Y, X - +Y, Y - +X, simplify = FALSE)
 fig1  <- set.edge.attribute(graph = fig1, name = "description", index = c(5, 6), value = "U")
 
-mat   <- matrix(nrow = 4,ncol = 2,byrow = T,
-                data = c(
-                  1, 1, # w
-                  0, 0, # x
-                  2, 0, # z
-                  2, 3  # y
-                ))
-
-row.names(mat) <- c("W", "X", "Z", "Y")
+#mat   <- matrix(nrow = 4,ncol = 2,byrow = T,
+#                data = c(
+#                  1, 1, # w
+#                  0, 0, # x
+#                  2, 0, # z
+#                  2, 3  # y
+#                ))
+#
+#row.names(mat) <- c("W", "X", "Z", "Y")
 
 NODES <- names(V(fig1))
 
@@ -36,160 +36,186 @@ ce1   <- causal.effect(y = "Y", x = "X", z = NULL, G = fig1, expr = TRUE, steps 
 alles <- ce1$steps
 ##########################################################################################################
 
-aio <- function() {
-
-tmptmp                     <- geparste_liste(quo(test))
-
-drive_pl <- upload_plots(igra_addr_eval       = tmptmp$igraph_addr_eval,
-                        platzh_namen_igraphs = tmptmp$platzhalter_namen_igraphs,
-                        mat                  = mat)
-
-map(tmptmp$expr_erw, ~eval_bare(.x, env = rlang::global_env()))
 
 
-alles  <- replace_null(alles, "nichts")
-alles  <- number_unnamed(alles)
+
+
+
+
 
 ##########################################################################################################
 
+aio <- function(schritte, mat = NULL) {
+  schritte <- enquo(schritte)
 
-werte      <- unlist(alles) %>% unname()
+  tmptmp   <- geparste_liste(schritte)
 
-hierarchie <- unlist(alles) %>% names() %>% stringr::str_replace_all("\\.", "/")
+  drive_pl <- upload_plots(
+    igra_addr_eval = tmptmp$igraph_addr_eval,
+    platzh_namen_igraphs = tmptmp$platzhalter_namen_igraphs,
+    mat = mat
+  )
 
+  map(tmptmp$expr_erw, ~ eval_bare(.x, env = rlang::global_env()))
 
-hierarchie <- hierarchie %>%
-  stringr::str_replace_all("children", "kinder") %>%
-  stringr::str_replace_all("root", "ende") %>%
-  stringr::str_replace_all("tree", "baum") %>%
-  stringr::str_replace_all("branch", "ast")
+  parat <- rlang::eval_tidy(schritte)
 
-hierarchie <- paste0("ausgang/", hierarchie)
+  parat    <- replace_null(parat, "nichts")
+  parat    <- listviewer::number_unnamed(parat)
 
-tib        <- tibble::tibble(pathString = hierarchie, werte = werte)
+  # ----------------------------
 
-daten <- data.tree::as.Node(tib)
+  werte      <- unlist(parat) %>% unname()
 
-daten$Set(nummer = 1:daten$totalCount)
-
-tmp <- data.tree::ToDataFrameTree(daten, "name", "pathString", "werte", "nummer") %>% dplyr::tbl_df()
-
-
-#################################
-
-lines <- tmp %>% dplyr::filter(name=="line") %>% dplyr::mutate(pathString=paste0(pathString,werte)) %>% dplyr::select(pathString)
-
-#################################
-
-tmp2 <- daten$Get("parent") %>% tibble::enframe() %>% dplyr::rename(parent = value)
-
-tmp2 <- tmp2 %>% dplyr::mutate(gparent = purrr::map(parent, ~try(doll(.x, "parent"), silent = T)))
+  hierarchie <- unlist(parat) %>% names() %>% stringr::str_replace_all("\\.", "/")
 
 
-tmp2 <- tmp2 %>% dplyr::mutate(name_parent  = purrr::map(parent,           ~try(doll(.x, "name"),           silent = T)),
-                               name_gparent = purrr::map(gparent,          ~try(doll(.x, "name"),           silent = T)),
-                               name_parent  = as.character(name_parent),
-                               name_gparent = as.character(name_gparent),
-                               path_parent  = purrr::map(parent,           ~try(doll(.x, "path"),           silent = T)),
-                               path_gparent = purrr::map(gparent,          ~try(doll(.x, "path"),           silent = T)),
-                               path_parent  = purrr::map_chr(path_parent,  ~try(paste0(.x, collapse = "/"), silent = T)),
-                               path_gparent = purrr::map_chr(path_gparent, ~try(paste0(.x, collapse = "/"), silent = T)))
+  hierarchie <- hierarchie %>%
+    stringr::str_replace_all("children", "kinder") %>%
+    stringr::str_replace_all("root", "ende") %>%
+    stringr::str_replace_all("tree", "baum") %>%
+    stringr::str_replace_all("branch", "ast")
+
+  hierarchie <- paste0("ausgang/", hierarchie)
+
+  tib        <- tibble::tibble(pathString = hierarchie, werte = werte)
+
+  daten <- data.tree::as.Node(tib)
+
+  daten$Set(nummer = 1:daten$totalCount)
+
+  tmp <- data.tree::ToDataFrameTree(daten, "name", "pathString", "werte", "nummer") %>% dplyr::tbl_df()
+
+  #################################
+
+  lines <- tmp %>% dplyr::filter(name=="line") %>% dplyr::mutate(pathString=paste0(pathString,werte)) %>% dplyr::select(pathString)
+
+  #################################
+
+  tmp2 <- daten$Get("parent") %>% tibble::enframe() %>% dplyr::rename(parent = value)
+
+  tmp2 <- tmp2 %>% dplyr::mutate(gparent = purrr::map(parent, ~try(doll(.x, "parent"), silent = T)))
 
 
-#################################
-
-tmp2 <- tmp2 %>% dplyr::select(name_parent:path_gparent)
-
-tmp  <- dplyr::bind_cols(tmp, tmp2)
-
-oo <- purrr::map_chr(c(tmptmp$platzhalter_namen_igraphs, NODES), ~or_filter("werte", .x)) %>%
-  paste0(collapse = " | ") %>%
-  rlang::parse_expr()
-
-
-tmp <- tmp %>%
-  dplyr::filter(!!oo) %>%
-  dplyr::mutate(ends_digit=stringr::str_ends(pathString, "[[:digit:]]")) %>%
-  dplyr::group_split(ends_digit) %>%
-  purrr::set_names(c("ends_digit_FALSE", "ends_digit_TRUE"))
-
-
-#################################
-
-tmp$ends_digit_TRUE <- tmp$ends_digit_TRUE %>%
-  dplyr::group_by(path_parent) %>%
-  tidyr::nest() %>%
-  dplyr::mutate(werte=purrr::map(data, ~ .x %>%
-                                   dplyr::pull(werte))) %>%
-  dplyr::mutate(werteB=purrr::map_chr(werte, ~paste0(.x, collapse = " "))) %>%
-  dplyr::filter(!stringr::str_ends(path_parent, "/v"))  %>%
-  dplyr::mutate(pathString = paste0(path_parent, "/", werteB))
-
-
-tmp$ends_digit_FALSE <- tmp$ends_digit_FALSE %>% dplyr::filter(!stringr::str_ends(pathString, "/v")) %>%
-  dplyr::mutate(pathString = paste0(pathString, "/", werte))
-
-#############################################
-
-tri <- drive_pl %>% dplyr::select(namen_igraphs, url)
-
-tri <-tri %>% dplyr::mutate(bild=br_img(url)) %>% dplyr::select(werte=namen_igraphs, bild)
-
-tmp$ends_digit_FALSE <- dplyr::left_join(tmp$ends_digit_FALSE,  tri, by="werte")
-
-
-#############################################
-
-tmp <- dplyr::bind_rows(
-  tmp$ends_digit_FALSE  %>% dplyr::select(pathString, bild),
-  tmp$ends_digit_TRUE   %>% dplyr::select(pathString),
-  lines)
-
-tmp <- tmp %>% dplyr::mutate(bild=dplyr::coalesce(bild, "<br><img src=''>"))
-
-#############################################
-
-
-tmp2 <- data.tree::as.Node(tmp)
-
-tmp <- data.tree::ToDataFrameTree(tmp2, "name", "pathString", "bild") %>%
-  dplyr::tbl_df() %>%
-  dplyr::mutate(bild=dplyr::coalesce(bild, "<br><img src=''>"))
-
-#############################################
+  tmp2 <- tmp2 %>% dplyr::mutate(name_parent  = purrr::map(parent,           ~try(doll(.x, "name"),           silent = T)),
+                                 name_gparent = purrr::map(gparent,          ~try(doll(.x, "name"),           silent = T)),
+                                 name_parent  = as.character(name_parent),
+                                 name_gparent = as.character(name_gparent),
+                                 path_parent  = purrr::map(parent,           ~try(doll(.x, "path"),           silent = T)),
+                                 path_gparent = purrr::map(gparent,          ~try(doll(.x, "path"),           silent = T)),
+                                 path_parent  = purrr::map_chr(path_parent,  ~try(paste0(.x, collapse = "/"), silent = T)),
+                                 path_gparent = purrr::map_chr(path_gparent, ~try(paste0(.x, collapse = "/"), silent = T)))
 
 
 
-fa <- colorspace::rainbow_hcl(9)
+  #################################
 
-tmp <- tmp %>%
-  dplyr::mutate(Farbe = dplyr::case_when(
+  tmp2 <- tmp2 %>% dplyr::select(name_parent:path_gparent)
 
-    !!!purrr::map(tmptmp$platzhalter_namen_igraphs, ~ patterns_lazy("name", .x, "cyan")),
+  tmp  <- dplyr::bind_cols(tmp, tmp2)
 
-    expr(stringr::str_detect(.data$name, "line") ~ "red"),
-
-    name == "cond"   ~  fa[1],
-    name == "var"    ~  fa[2],
-    name == "y"      ~  fa[3],
-    name == "x"      ~  fa[4],
-    name == "P"      ~  fa[5],
-    name == "sumset" ~  fa[6],
-    name == "s"      ~  fa[7],
-    name == "an"     ~  fa[8],
-    name == "G"      ~  fa[9],
-
-    TRUE ~ "white"))
+  oo <- purrr::map_chr(c(tmptmp$platzhalter_namen_igraphs, NODES), ~or_filter("werte", .x)) %>%
+    paste0(collapse = " | ") %>%
+    rlang::parse_expr()
 
 
-tmp2$Set(Farbe=tmp$Farbe)
-tmp2$Set(Bild =tmp$bild)
+  tmp <- tmp %>%
+    dplyr::filter(!!oo) %>%
+    dplyr::mutate(ends_digit=stringr::str_ends(pathString, "[[:digit:]]")) %>%
+    dplyr::group_split(ends_digit) %>%
+    purrr::set_names(c("ends_digit_FALSE", "ends_digit_TRUE"))
 
 
-tmp2
+  #################################
+
+  tmp$ends_digit_TRUE <- tmp$ends_digit_TRUE %>%
+    dplyr::group_by(path_parent) %>%
+    tidyr::nest() %>%
+    dplyr::mutate(werte=purrr::map(data, ~ .x %>%
+                                     dplyr::pull(werte))) %>%
+    dplyr::mutate(werteB=purrr::map_chr(werte, ~paste0(.x, collapse = " "))) %>%
+    dplyr::filter(!stringr::str_ends(path_parent, "/v"))  %>%
+    dplyr::mutate(pathString = paste0(path_parent, "/", werteB))
+
+
+  tmp$ends_digit_FALSE <- tmp$ends_digit_FALSE %>% dplyr::filter(!stringr::str_ends(pathString, "/v")) %>%
+    dplyr::mutate(pathString = paste0(pathString, "/", werte))
+
+
+  #############################################
+
+  tri <- drive_pl %>% dplyr::select(namen_igraphs, url)
+
+  tri <-tri %>% dplyr::mutate(bild=br_img(url)) %>% dplyr::select(werte=namen_igraphs, bild)
+
+  tmp$ends_digit_FALSE <- dplyr::left_join(tmp$ends_digit_FALSE,  tri, by="werte")
+
+
+  #############################################
+
+  tmp <- dplyr::bind_rows(
+    tmp$ends_digit_FALSE  %>% dplyr::select(pathString, bild),
+    tmp$ends_digit_TRUE   %>% dplyr::select(pathString),
+    lines)
+
+  tmp <- tmp %>% dplyr::mutate(bild=dplyr::coalesce(bild, "<br><img src=''>"))
+
+  #############################################
+
+
+  tmp2 <- data.tree::as.Node(tmp)
+
+  tmp <- data.tree::ToDataFrameTree(tmp2, "name", "pathString", "bild") %>%
+    dplyr::tbl_df() %>%
+    dplyr::mutate(bild=dplyr::coalesce(bild, "<br><img src=''>"))
+
+  #############################################
+
+
+
+  fa <- colorspace::rainbow_hcl(9)
+
+  tmp <- tmp %>%
+    dplyr::mutate(Farbe = dplyr::case_when(
+
+      !!!purrr::map(tmptmp$platzhalter_namen_igraphs, ~ patterns_lazy("name", .x, "cyan")),
+
+      expr(stringr::str_detect(.data$name, "line") ~ "red"),
+
+      name == "cond"   ~  fa[1],
+      name == "var"    ~  fa[2],
+      name == "y"      ~  fa[3],
+      name == "x"      ~  fa[4],
+      name == "P"      ~  fa[5],
+      name == "sumset" ~  fa[6],
+      name == "s"      ~  fa[7],
+      name == "an"     ~  fa[8],
+      name == "G"      ~  fa[9],
+
+      TRUE ~ "white"))
+
+
+  tmp2$Set(Farbe=tmp$Farbe)
+  tmp2$Set(Bild =tmp$bild)
+
+
+  tmp2
+
+
+
+
 
 }
 
+
+
+tester <- alles
+rm(alles)
+
+
+tmp2 <- aio(tester)
+
+##########################################################################################################
 
 collapsibleTree::collapsibleTree(tmp2,
                                  fill        = "Farbe",
